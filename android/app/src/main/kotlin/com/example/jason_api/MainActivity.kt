d2abbdaec3e002c294e251447bd1d0a46ae9897b
jncoderz <jncoderz@gmail.com>
2023-08-04 16:48:31 +0500

    rest api integrate in flutter framework

---
+++ android/app/src/main/kotlin/com/example/jason_api/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.jason_api
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:jason_api/home_page.dart';
+ void main(List<String> args) {
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false,
+    home: homePage(),
+    );
+  }
+}
